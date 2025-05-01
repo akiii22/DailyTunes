e8ee523a92226045782ceb672a2f46787da496c5
jeromee <bulosanjerome50@gmail.com>
2025-05-01 19:30:55 +0800

    Structuring the sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,27 @@
+import styled from "styled-components";
+
+const SideBarContainer = styled.div`
+  max-width: 270px;
+  border: 1px solid black;
+  height: 100vh;
+  padding: 1rem 2rem;
+`;
+
 function SideBar() {
   return (
-    <div>
-      <h1>SideBar</h1>
-    </div>
+    <SideBarContainer>
+      <h1>Daily Tunes</h1>
+      <div>
+        <ul>
+          <li>Dashboard</li>
+          <li>Habits</li>
+          <li>Accounts</li>
+          <li>Metrics</li>
+          <li>Settings</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+    </SideBarContainer>
   );
 }
 
